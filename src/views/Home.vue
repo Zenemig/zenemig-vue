1fa09dff2d90d6184d4da9a13a727ae35168ff63
Matías Giménez <matias.gimenez@ubiome.com>
2018-10-03 09:59:24 -0300

    chore(general): initial structure and files

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <img src="../assets/logo_zenemig.svg" width="200px" />
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  css: {
+    sourceMap: true,
+    extract: false
+  },
+
+  baseUrl: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  lintOnSave: undefined
+};
