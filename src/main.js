1069506cc2fc3e7252440f96e2bda12108d36ce4
Zenemig <hola@zenemig.net>
2019-01-23 16:53:46 -0300

    feat(fontawesome): define icons to use

--- src/main.js
+++ src/main.js
@@ -2,6 +2,37 @@ import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
 import router from "./router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faVuejs,
+  faMedium,
+  faGithub,
+  faLinkedin,
+  faTwitter,
+  faHtml5
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faHeart,
+  faTimesCircle,
+  faBars,
+  faBriefcase,
+  faGlobeAmericas
+} from "@fortawesome/pro-light-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faVuejs);
+library.add(faMedium);
+library.add(faGithub);
+library.add(faLinkedin);
+library.add(faTwitter);
+library.add(faHtml5);
+library.add(faHeart);
+library.add(faTimesCircle);
+library.add(faBars);
+library.add(faBriefcase);
+library.add(faGlobeAmericas);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
