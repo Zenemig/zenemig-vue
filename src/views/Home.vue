613f8551c4f4ec84c5312f40d1d05ee27f785598
Zenemig <hola@zenemig.net>
2019-01-26 00:17:43 -0300

    feat(index): link to google fonts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <nav id="nav-mobile">
-      <!-- <img
+      <img
         src="../assets/logo_zenemig.svg"
         class="nav-mobile-logo"
         alt="Zenemig Logo"
-        title="Zenemig Logo"> -->
+        title="Zenemig Logo">
       <font-awesome-icon :icon="['fab', 'twitter']" @click="toggleTwitterSidebar" />
       <font-awesome-icon class="nav-mobile-icn-menu" :icon="['fal', 'bars']" @click="toggleMenuSidebar" />
     </nav>
