4f4301d653a3a4d6226250fc5b1fbe2cad8c6d87
Zenemig <hola@zenemig.net>
2019-01-27 20:33:18 -0300

    refactor(home): create project component and restructure html and css

---
+++ src/components/project.vue
@@ -0,0 +1,55 @@
+<template>
+  <article class="c-project">
+    <div class="c-project__img" :class="project.image_class">
+      <div class="h-overlay-black-medium" />
+      <div class="c-project__icon">
+        <font-awesome-icon :icon="['fab', project.icon]" />
+      </div>
+    </div>
+
+    <div class="c-project__tags">
+      <font-awesome-icon :icon="['fal', 'tags']" class="icon" /> {{ getTags(project.tags) }}
+    </div>
+
+    <h2 class="c-project__title">
+      {{ project.title }}
+    </h2>
+
+    <p class="c-project__description">
+      {{ project.description }}
+    </p>
+
+    <div class="c-project__footer">
+      <div class="employer">
+        <font-awesome-icon :icon="['fal', 'briefcase']" class="icon" /> Cliente:
+        <a :href="project.employer.url">
+          {{ project.employer.name }}
+        </a>
+      </div>
+
+      <div class="project-url">
+        <font-awesome-icon :icon="['fal', 'globe-americas']" class="icon" />
+        <a :href="project.project.url">
+          Visita el Proyecto
+        </a>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'project',
+  props: {
+    project: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    getTags: function (arr) {
+      return arr.join(' | ')
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,190 +1,89 @@
 <template>
   <section
-    id="content"
-    class="content">
-    <h1 class="headline intro">
-      Hola, soy Matías Giménez, con G de gato.
+    class="v-home">
+    <h1 class="c-headline">
+      Hola, soy Matías Giménez, con G de gato
       <br><small>Padre de Amelia y Lucca, compañero de aventuras de <a href="http://alessandrazanocco.com">Alessandra Zanocco</a> y desarrollador Front-End</small>
     </h1>
 
-    <div class="content-box">
-
-      <article class="content-work-card">
-        <div class="content-work-card-img escapararte">
-          <div class="overlay-black-medium"/>
-          <span class="">
-            <font-awesome-icon :icon="['fab', 'html5']" />
-          </span>
-        </div>
-
-        <ul class="content-work-card-detail">
-          <li>UX</li>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>jQuery</li>
-        </ul>
-
-        <h2 class="content-work-card-title">EscaparArte</h2>
-
-        <p class="content-work-card-text">Maqueta para el sitio de <a href="http://escapararte.cl">EscaparArte</a>.</p>
-
-        <div class="content-work-card-footer">
-          <div class="employer">
-            <a href="https://merca.cl"><font-awesome-icon :icon="['fal', 'briefcase']" />Merca</a>
-          </div>
-
-          <div class="project-url">
-            <a href="http://www.clientes.zenemig.net/Merca/Escapararte/"><font-awesome-icon :icon="['fal', 'globe-americas']" />EscaparArte</a>
-          </div>
-        </div>
-      </article>
-
-      <article class="content-work-card">
-        <div class="content-work-card-img resostenido">
-          <div class="overlay-black-medium"/>
-          <span class="fa fa-stack fa-4x">
-            <i class="fa fa-circle fa-stack-2x"/>
-            <i class="fa fa-html5 fa-stack-1x"/>
-          </span>
-        </div>
-
-        <ul class="content-work-card-detail">
-          <li>UX</li>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>jQuery</li>
-        </ul>
-
-        <h2 class="content-work-card-title">Resostenido</h2>
-
-        <p class="content-work-card-text">Maqueta para el sitio de <a href="http://resostenido.cl">Resostenido</a>.</p>
-
-        <div class="content-work-card-footer">
-          <div class="employer">
-            <a href="https://merca.cl"><i class="fa fa-briefcase"/>Merca</a>
-          </div>
-
-          <div class="project-url">
-            <a href="http://www.clientes.zenemig.net/Merca/Resostenido/"><i class="fa fa-globe"/>Resostenido</a>
-          </div>
-        </div>
-      </article>
-
-      <article class="content-work-card">
-        <div class="content-work-card-img zenemig">
-          <div class="overlay-black-medium"/>
-          <span class="fa fa-stack fa-4x">
-            <i class="fa fa-circle fa-stack-2x"/>
-            <i class="fa fa-wordpress fa-stack-1x"/>
-          </span>
-        </div>
-
-        <ul class="content-work-card-detail">
-          <li>AI</li>
-          <li>UX</li>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>jQuery</li>
-          <li>WP</li>
-        </ul>
-
-        <h2 class="content-work-card-title">Zenemig</h2>
-
-        <p class="content-work-card-text">Portafolio y blog personal.</p>
-
-        <div class="content-work-card-footer">
-          <div class="employer">
-            <a href="https://zenemig.net"><i class="fa fa-briefcase"/>Zenemig</a>
-          </div>
-
-          <div class="project-url">
-            <a href="http://zenemig.net"><i class="fa fa-globe"/>Zenemig</a>
-          </div>
-        </div>
-      </article>
-
-      <article class="content-work-card">
-        <div class="content-work-card-img manetch">
-          <div class="overlay-black-medium"/>
-          <span class="fa fa-stack fa-4x">
-            <i class="fa fa-circle fa-stack-2x"/>
-            <i class="fa fa-html5 fa-stack-1x"/>
-          </span>
-        </div>
-
-        <ul class="content-work-card-detail">
-          <li>AI</li>
-          <li>UX</li>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>jQuery</li>
-        </ul>
-
-        <h2 class="content-work-card-title">Sitio de Eventos</h2>
-
-        <p class="content-work-card-text">Plantilla sobre Ruby on Rails para los eventos de <a href="https://manetch.com">manetch</a>.</p>
-
-        <div class="content-work-card-footer">
-          <div class="employer">
-            <a href="https://manetch.com"><i class="fa fa-briefcase"/>manetch</a>
-          </div>
-
-          <div class="project-url">
-            <a href="https://manetch.com/events/wcdgt"><i class="fa fa-globe"/>WCDT</a>
-          </div>
-        </div>
-      </article>
-
-      <article class="content-work-card">
-        <div class="content-work-card-img volviendo-a-la-tierra">
-          <div class="overlay-black-medium"/>
-          <span class="fa fa-stack fa-4x">
-            <i class="fa fa-circle fa-stack-2x"/>
-            <i class="fa fa-html5 fa-stack-1x"/>
-          </span>
-        </div>
-
-        <ul class="content-work-card-detail">
-          <li>AI</li>
-          <li>UX</li>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>jQuery</li>
-        </ul>
-
-        <h2 class="content-work-card-title">Volviendo a la Tierra</h2>
-
-        <p class="content-work-card-text">Página de caída estática hecha para la venta del libro <strong>Volviendo a la Tierra</strong>.</p>
-
-        <div class="content-work-card-footer">
-          <div class="employer">
-            <a href="https://zenemig.net"><i class="fa fa-briefcase"/>Zenemig</a>
-          </div>
-
-          <div class="project-url">
-            <a href="http://volviendoalatierra.cl"><i class="fa fa-globe"/>Volviendo a la Tierra</a>
-          </div>
-        </div>
-      </article>
+    <div class="c-portfolio">
+      <project v-for="project in portfolio" :key="project.title" :project="project" />
     </div>
-
-    <div class="clearfix"/>
-    <footer class="content-footer-mobile">
-      Hecho con <i class="fa fa-heart"/> y <a href="http://wordpress.org"><i class="fa fa-wordpress"/></a>
-    </footer>
   </section>
 </template>
 
 <script>
+import project from '@/components/project'
 export default {
   name: 'Home',
+  components: {
+    project
+  },
   data: () => {
     return {
-
+      portfolio: [
+        {
+          icon: 'vuejs',
+          image_class: 'zenemig',
+          tags: ['UX', 'UI', 'HTML5', 'Sass', 'JS', 'Vue', 'Netlify'],
+          title: 'Zenemig',
+          description: 'Portafolio personal.',
+          employer: {
+            name: 'Zenemig',
+            url: 'https://zenemig.net'
+          },
+          project: {
+            name: 'Zenemig',
+            url: 'https://zenemig.net'
+          }
+        },
+        {
+          icon: 'html5',
+          image_class: 'manetch',
+          tags: ['UX', 'UI', 'HTML5', 'CSS', 'jQuery'],
+          title: 'Plantilla de Eventos',
+          description: 'Plantilla sobre Ruby on Rails para los eventos de manetch.',
+          employer: {
+            name: 'manetch',
+            url: 'https://manetch.com'
+          },
+          project: {
+            name: 'WCDT',
+            url: 'https://www.manetch.com/events/wcdt+i19'
+          }
+        },
+        {
+          icon: 'html5',
+          image_class: 'escapararte',
+          tags: ['UX', 'HTML5', 'Sass', 'jQuery'],
+          title: 'EscaparArte',
+          description: 'Maqueta para el sitio de EscaparArte.',
+          employer: {
+            name: 'Merca',
+            url: 'https://merca.cl'
+          },
+          project: {
+            name: 'EscaparArte',
+            url: 'http://www.clientes.zenemig.net/Merca/Escapararte/'
+          }
+        },
+        {
+          icon: 'html5',
+          image_class: 'resostenido',
+          tags: ['UX', 'HTML5', 'Sass', 'jQuery'],
+          title: 'Resostenido',
+          description: 'Maqueta para el sitio de Resostenido.',
+          employer: {
+            name: 'Merca',
+            url: 'https://merca.cl'
+          },
+          project: {
+            name: 'Resostenido',
+            url: 'http://www.clientes.zenemig.net/Merca/Resostenido/'
+          }
+        }
+      ]
     }
-  },
-  methods: {
-
   }
 }
 </script>
