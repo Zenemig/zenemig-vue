f93f58db87c6ffba11d7a884c9254c8096ba174e
Zenemig <hola@zenemig.net>
2019-01-28 15:29:01 -0300

    refactor(twitter-sidebar): add header to sidebar

--- src/components/twitter-sidebar.vue
+++ src/components/twitter-sidebar.vue
@@ -2,16 +2,24 @@
   <aside
     class="l-sidebar l-sidebar--right c-twitter-sidebar"
     :class="{'active': show}">
-    <button class="l-sidebar__close" @click="toggleSidebar('twitter')">
-      <font-awesome-icon
-        :icon="['fal', 'times-circle']"
-      />
-    </button>
-
     <button class="c-twitter-sidebar__toggle-button"  @click="toggleSidebar('twitter')">
       <font-awesome-icon v-if="!show" :icon="['fab', 'twitter']" />
       <font-awesome-icon v-else :icon="['fal', 'times']" />
     </button>
+
+    <header class="c-twitter-sidebar__header">
+      <h2 class="c-twitter-sidebar__title">
+        <a href="https://twitter.com/zenemig">
+          @zenemig
+        </a>
+      </h2>
+
+      <button class="l-sidebar__close" @click="toggleSidebar('twitter')">
+        <font-awesome-icon
+          :icon="['fal', 'times-circle']"
+        />
+      </button>
+    </header>
 
     <div class="c-twitter-sidebar__feed">
       <a class="twitter-timeline" href="https://twitter.com/zenemig?ref_src=twsrc%5Etfw">
