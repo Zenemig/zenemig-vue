4adbbab76a6731d9838bfae2e307df8d3d1138ef
Zenemig <hola@zenemig.net>
2019-01-28 14:10:37 -0300

    refactor(twitter-sidebar): use new twitter timeline code

--- src/components/twitter-sidebar.vue
+++ src/components/twitter-sidebar.vue
@@ -13,22 +13,15 @@
       <font-awesome-icon v-else :icon="['fal', 'times']" />
     </button>
 
-    <h3 class="sidebar-twitter-title"><a href="http://twitter.com">@zenemig</a></h3>
-    <div id="sidebar-twitter-feed">
-      <a
-        class="twitter-timeline"
-        href="https://twitter.com/zenemig"
-        data-widget-id="504771579041943553"
-        width="320"
-        data-chrome="noborders noheader nofooter">Tweets by @zenemig</a>
-      <div class="tweet_footer"/>
+    <div class="c-twitter-sidebar__feed">
+      <a class="twitter-timeline" href="https://twitter.com/zenemig?ref_src=twsrc%5Etfw">
+        Tweets by zenemig
+      </a>
     </div>
   </aside>
 </template>
 
 <script>
-// Twitter timeline
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 export default {
   name: 'twitterSidebar',
   props: {
