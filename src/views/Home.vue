1c1afcec36c6a74eadce3be13936428a2b710659
Zenemig <hola@zenemig.net>
2019-01-31 12:26:49 -0300

    refactor(home): remove portfolio object from data object

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,13 +7,77 @@
     </h1>
 
     <div class="c-portfolio">
-      <project v-for="project in portfolio" :key="project.title" :project="project" />
+      <project v-for="project in projects" :key="project.title" :project="project" />
     </div>
   </section>
 </template>
 
 <script>
 import project from '@/components/project'
+
+const portfolio = [
+  {
+    icon: 'vuejs',
+    image_class: 'zenemig',
+    tags: ['UX', 'UI', 'HTML5', 'Sass', 'JS', 'Vue', 'Netlify'],
+    title: 'Zenemig',
+    description: 'Portafolio personal.',
+    employer: {
+      name: 'Zenemig',
+      url: 'https://zenemig.net'
+    },
+    project: {
+      name: 'Zenemig',
+      url: 'https://github.com/zenemig'
+    }
+  },
+  {
+    icon: 'html5',
+    image_class: 'manetch',
+    tags: ['UX', 'UI', 'HTML5', 'CSS', 'jQuery'],
+    title: 'Plantilla de Eventos',
+    description: 'Plantilla sobre Ruby on Rails para los eventos de manetch.',
+    employer: {
+      name: 'manetch',
+      url: 'https://manetch.com'
+    },
+    project: {
+      name: 'WCDT',
+      url: 'https://www.manetch.com/events/wcdt+i19'
+    }
+  },
+  {
+    icon: 'html5',
+    image_class: 'escapararte',
+    tags: ['UX', 'HTML5', 'Sass', 'jQuery'],
+    title: 'EscaparArte',
+    description: 'Maqueta para el sitio de EscaparArte.',
+    employer: {
+      name: 'Merca',
+      url: 'https://merca.cl'
+    },
+    project: {
+      name: 'EscaparArte',
+      url: 'https://escapararte.netlify.com/'
+    }
+  },
+  {
+    icon: 'html5',
+    image_class: 'resostenido',
+    tags: ['UX', 'HTML5', 'Sass', 'jQuery'],
+    title: 'Resostenido',
+    description: 'Maqueta para el sitio de Resostenido.',
+    employer: {
+      name: 'Merca',
+      url: 'https://merca.cl'
+    },
+    project: {
+      name: 'Resostenido',
+      url: 'https://resostenido.netlify.com/'
+    }
+  }
+]
+
 export default {
   name: 'Home',
   components: {
@@ -21,68 +85,7 @@ export default {
   },
   data: () => {
     return {
-      portfolio: [
-        {
-          icon: 'vuejs',
-          image_class: 'zenemig',
-          tags: ['UX', 'UI', 'HTML5', 'Sass', 'JS', 'Vue', 'Netlify'],
-          title: 'Zenemig',
-          description: 'Portafolio personal.',
-          employer: {
-            name: 'Zenemig',
-            url: 'https://zenemig.net'
-          },
-          project: {
-            name: 'Zenemig',
-            url: 'https://github.com/zenemig'
-          }
-        },
-        {
-          icon: 'html5',
-          image_class: 'manetch',
-          tags: ['UX', 'UI', 'HTML5', 'CSS', 'jQuery'],
-          title: 'Plantilla de Eventos',
-          description: 'Plantilla sobre Ruby on Rails para los eventos de manetch.',
-          employer: {
-            name: 'manetch',
-            url: 'https://manetch.com'
-          },
-          project: {
-            name: 'WCDT',
-            url: 'https://www.manetch.com/events/wcdt+i19'
-          }
-        },
-        {
-          icon: 'html5',
-          image_class: 'escapararte',
-          tags: ['UX', 'HTML5', 'Sass', 'jQuery'],
-          title: 'EscaparArte',
-          description: 'Maqueta para el sitio de EscaparArte.',
-          employer: {
-            name: 'Merca',
-            url: 'https://merca.cl'
-          },
-          project: {
-            name: 'EscaparArte',
-            url: 'https://escapararte.netlify.com/'
-          }
-        },
-        {
-          icon: 'html5',
-          image_class: 'resostenido',
-          tags: ['UX', 'HTML5', 'Sass', 'jQuery'],
-          title: 'Resostenido',
-          description: 'Maqueta para el sitio de Resostenido.',
-          employer: {
-            name: 'Merca',
-            url: 'https://merca.cl'
-          },
-          project: {
-            name: 'Resostenido',
-            url: 'https://resostenido.netlify.com/'
-          }
-        }
-      ]
+      projects: portfolio
     }
   }
 }
