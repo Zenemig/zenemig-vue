381d91a36d792d3b9a44f9516a10fbc571ee9a53
Zenemig <hola@zenemig.net>
2020-03-25 01:20:26 -0300

    fix(instagram-feed): upgrade to use cdn

--- src/components/twitter-sidebar.vue
+++ src/components/twitter-sidebar.vue
@@ -23,7 +23,7 @@
     </header>
 
     <div class="c-twitter-sidebar__feed">
-      <iframe src="//lightwidget.com/widgets/67517fd20fec591ca35a1bed9e1d2d53.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
+      <iframe src="https://cdn.lightwidget.com/widgets/67517fd20fec591ca35a1bed9e1d2d53.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
       <!-- <a class="twitter-timeline" href="https://twitter.com/zenemig?ref_src=twsrc%5Etfw">
         Tweets by zenemig
       </a> -->
